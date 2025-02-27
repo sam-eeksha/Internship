04663c7e97b76b8964f9a89ff06bba87070496c7
Sameeksha <mssameeksha@gmail.com>
2025-02-27 14:08:42 +0530

    Video 2 Completed

---
+++ Sameeksha/Day5/index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//import mongoose from 'mongoose';
+//console.log("Hello there");
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+// app.get('/api/products',async(req,res)=>{
+//     try{
+//         const products=await Product.find({});
+//         res.status(200).json(products);
+//     }
+//     catch(error){
+//         res.status(500).json({message:error.message});
+//     }
+// });
+
+// app.get('/api/products/:id', async (req,res)=>{
+//     try{
+//         const {id}=req.params;
+//         const product=await Product.findById(id);
+//         res.status(200).json(product);
+//     }
+//     catch(error){
+//         res.status(500).json({message:error.message});
+//     }
+// });
+// app.post('/api/products', async(req,res)=>{
+//     try{
+//         const product=await Product.create(req.body);
+//         res.status(200).json(product);
+//     }
+//     catch(error){
+//         res.status(500).json({message:error.message});
+//     }
+// });
+// app.put('/api/products/:id', async (req,res)=>{
+//     try{
+//         const {id}=req.params;
+//         const product=await Product.findByIdAndUpdate(id,req.body);
+//         if(!product){
+//             return res.status(404).json({message:"Product not found"});
+//         }
+//         const UpdatedProduct = await Product.findById(id);
+//         res.status(200).json(UpdatedProduct);
+//     }
+//     catch(error){
+//         res.status(500).json({message:error.message});
+//     }
+// })
+
+// app.delete('/api/products/:id', async(req,res)=>{
+//     try{
+//         const {id}=req.params;
+//         const product=await Product.findByIdAndDelete(id);
+
+//         if(!product){
+//             return res.status(404).json({message:'Product not found'});
+//         }
+//         res.status(200).json({message:'Product deleted sucessfully'});
+
+//     }
+//     catch(error){
+//         res.status(500).json({message:error.message});
+//     }
+// })
+
+mongoose
+  .connect(
+    "mongodb+srv://Sameeksha:1132003sam@backenddb.rg9up.mongodb.net/Video2?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Database connection failed");
+  });
